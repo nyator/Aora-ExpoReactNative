91afa1b55765275db68809c2803676a5b8def0b9
Henry <hhen5562@gmail.com>
2024-05-26 18:24:36 +0000

    Pages Structure

--- app/index.jsx
+++ app/index.jsx
@@ -2,10 +2,10 @@ import { Text, View } from "react-native";
 import { StatusBar } from "react-native";
 import { Link } from "expo-router";
 
-export default function Page() {
+export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-    <Text className='text-3xl'>Aora</Text>
+    <Text className='text-3xl font-pblack'>Aora</Text>
     <StatusBar style="auto" />
     <Link href='/profile' className="text-blue-500">Profile</Link>
   </View>
