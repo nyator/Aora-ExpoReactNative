1c1b6e936b03dc2b333f97bc3db39fbd922e1a59
Henry <hhen5562@gmail.com>
2024-05-26 21:54:37 +0000

    Tabs layout

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
     <Text className='text-3xl font-pblack'>Aora</Text>
     <StatusBar style="auto" />
-    <Link href='/profile' className="text-blue-500">Profile</Link>
+    <Link href='/home' className="text-blue-500 text-2xl">Home</Link>
   </View>
 );
 }
